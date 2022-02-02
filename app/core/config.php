622b81b0297f8e8f5a0898f7f3bb72ab71b0e38a
Zoltan Horvath <zoltan.horvath@proxybit.hu>
2022-02-02 20:17:30 +0100

    add config constants

--- app/core/config.php
+++ app/core/config.php
@@ -1,5 +1,39 @@
 <?php
 
-/* Website title */
+/* WEBSITE TITLE */
 
 define('APP_NAME', 'Ad listing app');
+
+/* DATABASE CREDENTIALS */
+
+define('DB_HOST', 'localhost');
+define('DB_TYPE', 'mysql');
+define('DB_NAME', 'ad_listing_app');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '1234');
+
+/* PROTOCOL TYPE */
+
+define('PROTOCOL', 'http');
+
+/* ROOT AND ASSET PATHS */
+
+$path = str_replace("\\", "/", PROTOCOL ."://" . $_SERVER['SERVER_NAME'] . __DIR__  . "/");
+$path = str_replace($_SERVER['DOCUMENT_ROOT'], "", $path);
+
+define('ROOT', str_replace("app/core", "public", $path));
+define('ASSETS', str_replace("app/core", "public/assets", $path));
+
+/* ERROR REPORTING */
+
+define('DEBUG',true);
+
+if(DEBUG)
+{
+	ini_set("display_errors",1);
+}else{
+	ini_set("display_errors",0);
+}
+
+
+
