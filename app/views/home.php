c71153c2865eee5c653d6b0ce5a94564eae5c7c8
Zoltan Horvath <zoltan.horvath@proxybit.hu>
2022-02-03 16:49:50 +0100

    add basic html structure

--- app/views/home.php
+++ app/views/home.php
@@ -1,1 +1,49 @@
-This is the home page
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home Page</title>
+</head>
+<body>
+  
+  <!-- CONTENT WRAPPER -->
+
+  <div>
+
+   <!-- NAVBAR -->
+
+    <nav>
+      
+    </nav>
+
+    <!-- NAVBAR ENDS -->
+
+    <!-- MAIN SECTION -->
+
+    <section>
+
+    </section>
+
+    <!-- MAIN SECTION ENDS -->
+
+    <!-- FOOTER -->
+
+    <footer>
+
+    </footer>
+
+    <!-- FOOTER ENDS -->
+
+  </div>
+
+  <!-- CONTENT WRAPPER ENDS -->
+
+  <!-- SCRIPTS -->
+
+  <script></script>
+
+  <!-- SCRIPTS END -->
+</body>
+</html>
