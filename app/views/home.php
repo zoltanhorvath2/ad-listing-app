00e9bb6cc0569c3255051c63361d521e7fa97326
Zoltan Horvath <zoltan.horvath@proxybit.hu>
2022-02-03 17:47:03 +0100

    add basic navbar and footer styles

--- app/views/home.php
+++ app/views/home.php
@@ -5,6 +5,14 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home Page</title>
+  <!-- Fontawesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+  <!-- Google fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;400&display=swap" rel="stylesheet">
+  <!-- Custom styles -->
+  <link rel="stylesheet" href="../../../ad-listing-app/public/css/app.css">
 </head>
 <body>
   
@@ -14,8 +22,15 @@
 
    <!-- NAVBAR -->
 
-    <nav>
-      
+    <nav id="main_navbar">
+      <div>
+        <i class="fas fa-ad ad-icon"></i>
+      </div>
+      <ul class="nav-items-list">
+        <a href="" class="nav-item"><li>Home</li></a>
+        <a href="" class="nav-item"><li>Users</li></a>
+        <a href="" class="nav-item"><li>Ads</li></a>
+      </ul>
     </nav>
 
     <!-- NAVBAR ENDS -->
@@ -30,8 +45,10 @@
 
     <!-- FOOTER -->
 
-    <footer>
-
+    <footer class="footer">
+      <div class="footer-text-containter">
+        <h4>Zoltan Horvath &copy; 2022</h4>
+      </div>
     </footer>
 
     <!-- FOOTER ENDS -->
