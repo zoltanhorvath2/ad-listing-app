b4af741bfd382e1e706e397476ac57543e5fcb13
Zoltan Horvath <zoltan.horvath@proxybit.hu>
2022-02-04 16:06:08 +0100

    add installation guide

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ad-listing-app
-This is a small MVC structured application using no frameworks at all (JavaScript, CSS, PHP, MySQL)
+This is a small MVC structured application using no frameworks at all (CSS, PHP, MySQL)
+
+Installation guide for Advertisement Listing App:
+
+Prequisities:
+
+- PHP 8.0.11 <
+- MySQL  Ver 8.0.26 <
+- Apache/2.4.46
+- Highly recommmended: XAMPP 8.0.12 < (because the three above come with it in a bundle
+
+Installation:
+
+- Download XAMPP if you haven’t got it on your machine
+- Navigate to ‘hotdocs’ folder in your terminal
+- Run ‘git clone https://github.com/zoltanhorvath2/ad-listing-app.git'
+- Open your mysql from command line or phpmyadmin (for example http://localhost:70/phpmyadmin)
+- Create a database named ‘ad_listing_app’ and import database from ad_listing_app.sql in root folder or run the queries manually
+- Open the project on the correct port in your browser (for example: http://localhost:70/ad-listing-app/public/)
--- ad_listing_app.sql
+++ ad_listing_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 04, 2022 at 03:47 PM
+-- Generation Time: Feb 04, 2022 at 04:01 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -33,6 +33,20 @@ CREATE TABLE `ads` (
   `title` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `ads`
+--
+
+INSERT INTO `ads` (`id`, `userid`, `title`) VALUES
+(7, 16, 'new advertisement 1'),
+(8, 16, 'new advertisement 2'),
+(9, 17, 'new advertisement 3'),
+(10, 15, 'new advertisement 4'),
+(11, 15, 'new advertisement 5'),
+(12, 15, 'new advertisement 6'),
+(13, 18, 'new advertisement 7'),
+(14, 18, 'new advertisement 8');
+
 -- --------------------------------------------------------
 
 --
@@ -43,6 +57,16 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`) VALUES
+(16, 'leonardo'),
+(17, 'michelangelo'),
+(15, 'picasso'),
+(18, 'rafaello');
 
 --
 -- Indexes for dumped tables
@@ -70,13 +94,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `ads`
 --
 ALTER TABLE `ads`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Constraints for dumped tables
