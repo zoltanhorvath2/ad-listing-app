78fb478c107793afd25757293077625539859b53
Zoltan Horvath <zoltan.horvath@proxybit.hu>
2022-02-04 21:58:21 +0100

    add comments to the models

--- app/models/ad.php
+++ app/models/ad.php
@@ -2,7 +2,11 @@
 
 class Ad extends Model
 {
-  
+  /**
+   * Fetch all ads from database
+   * 
+   * @return array|null
+   */
   public function getAllAds(): ?array
   {
     return $this->read("SELECT ads.id, ads.title, users.username FROM ads JOIN users ON ads.userid = users.id ORDER BY ads.id ASC");
--- app/models/user.php
+++ app/models/user.php
@@ -2,13 +2,23 @@
 
 class User extends Model
 {
-  
+  /**
+   * Fetch all users from database
+   * 
+   * @return array|null
+   */
   public function getAllUsers(): ?array
   {
     return $this->read('SELECT * FROM users ORDER BY id ASC');
   }
-
-  public function addUser($username): bool
+  /**
+   * Creates a new user record
+   * 
+   * @param string
+   * 
+   * @return bool
+   */
+  public function addUser(string $username): bool
   {
     return $this->write('INSERT INTO users (username) VALUES (:username)', ['username' => $username]);
   }
